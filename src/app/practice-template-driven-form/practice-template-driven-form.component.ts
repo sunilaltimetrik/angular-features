fdddd6f2c77fc7b5b3034a833bc1821879d76b80
sunilaltimetrik <sonisunil486@gmail.com>
2019-08-28 15:41:51 +0530

    added tamplet driven form practice code

--- src/app/practice-template-driven-form/practice-template-driven-form.component.ts
+++ src/app/practice-template-driven-form/practice-template-driven-form.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
 
 @Component({
   selector: 'practice-template-driven-form',
@@ -6,10 +8,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./practice-template-driven-form.component.css']
 })
 export class PracticeTemplateDrivenFormComponent implements OnInit {
+  categoryObj = [
+    {id: 1, value: 'Development'},
+    {id: 2, value: 'Testing'},
+    {id: 3, value: 'Other'}
 
+  ]
   constructor() { }
 
   ngOnInit() {
   }
-
+  onSubmit(formData: NgForm){
+    console.log('Form Data = ' + formData.value);
+  }
 }
